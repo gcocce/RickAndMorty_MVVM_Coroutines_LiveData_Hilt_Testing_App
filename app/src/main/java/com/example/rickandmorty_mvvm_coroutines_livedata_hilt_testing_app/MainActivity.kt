99a6529d711ec40bfe81866c628070533a136647
Gaston Cocce <gcocce@gmail.com>
2022-02-08 20:59:33 -0300

    Create Project

---
+++ app/src/main/java/com/example/rickandmorty_mvvm_coroutines_livedata_hilt_testing_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rickandmorty_mvvm_coroutines_livedata_hilt_testing_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
